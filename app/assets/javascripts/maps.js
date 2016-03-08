681d0ea302557730b874b5b0f85c1360e7e04ad4
AnkitGeorge <ankit.george@gmail.com>
2016-03-08 18:16:58 -0500

    Nearby markers on potholes working.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,17 +1,27 @@
-function initMap() {
-  var mapPlaceholder = $('#map');
+$(document).on('ready page:load', function() {
+ var mapPlaceholder = $('#map');
+ var latLong = {lat: mapPlaceholder.data('latitude'), lng: mapPlaceholder.data('longitude')}
+ var customIcon = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
 
-  var latLong = {lat: mapPlaceholder.data('latitude'), lng: mapPlaceholder.data('longitude')};
+ var map = new google.maps.Map(mapPlaceholder[0], {
+   center: latLong,
+   zoom: 17
+ });
 
-  var map = new google.maps.Map(mapPlaceholder[0], {
-    center: latLong,
-    zoom: 15
+ var marker = new google.maps.Marker({
+   position: latLong,
+   map: map,
+   title: 'Current_pothole'
+ });
 
-  }); //details for loading the map on the page
-
-  var marker = new google.maps.Marker({
-    position: latLong,
-    map: map,
-    title: 'Pothole!',
-  });
-}
+ if (window.nearbys) {
+   nearbys.forEach(function(coord) {
+     new google.maps.Marker({
+       position: { lat: parseFloat(coord.lat), lng: parseFloat(coord.lng) },
+       map: map,
+       title: "Nearby_potholes",
+       icon: customIcon
+     });
+   });
+ }
+});

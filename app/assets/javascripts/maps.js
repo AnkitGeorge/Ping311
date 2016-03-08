ac31c53896993dafdbcdead935510253618673a8
AnkitGeorge <ankit.george@gmail.com>
2016-03-08 17:38:35 -0500

    Got marker on maps to work.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,9 +1,17 @@
 function initMap() {
   var mapPlaceholder = $('#map');
 
+  var latLong = {lat: mapPlaceholder.data('latitude'), lng: mapPlaceholder.data('longitude')};
+
   var map = new google.maps.Map(mapPlaceholder[0], {
-    center: {lat: mapPlaceholder.data('latitude'), lng: mapPlaceholder.data('longitude')},
+    center: latLong,
     zoom: 15
 
   }); //details for loading the map on the page
+
+  var marker = new google.maps.Marker({
+    position: latLong,
+    map: map,
+    title: 'Pothole!',
+  });
 }
